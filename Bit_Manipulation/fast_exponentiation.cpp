2a35483dc4fe0fd9e5a863227719d04a146a0b52
kmrakash <aksh.iem22@gmail.com>
2021-03-19 18:15:45 +0530

    fast exponentiation recursive approach

--- Bit_Manipulation/fast_exponentiation.cpp
+++ Bit_Manipulation/fast_exponentiation.cpp
@@ -1,9 +1,16 @@
+/*
+    * To compute efficiently base(x) to the power(y) || x^y
+    * time complexity : O(log(power))
+    * Implementation of recursive and bit masking approach
+*/
+
 #include <iostream>
 #include <cassert>
 using namespace std;
 
 #define lli long long int
 
+// using Bit Masking
 lli exponentiation(lli base, lli power)
 {
 
@@ -21,14 +28,45 @@ lli exponentiation(lli base, lli power)
     return ans;
 }
 
+// Recursive Approach
+lli exponentiation2(lli base, lli power)
+{
+
+    lli temp;
+    if (power == 0)
+        return 1;
+
+    temp = exponentiation2(base, power / 2);
+
+    if (power % 2 == 0)
+    {
+        return temp * temp;
+    }
+    else
+    {
+        if (power > 0)
+            return base * temp * temp;
+        else
+            return (temp * temp) / base; // handle negative power
+    }
+}
+
 int main()
 {
+    // validating with some Test cases
     assert(exponentiation(3, 5) == 243);
     assert(exponentiation(5, 3) == 125);
     assert(exponentiation(2, 6) == 64);
     assert(exponentiation(789, 9) == 6247726042369092341);
+
+    assert(exponentiation2(3, 5) == 243);
+    assert(exponentiation2(5, 3) == 125);
+    assert(exponentiation2(2, 6) == 64);
+    assert(exponentiation2(789, 9) == 6247726042369092341);
+
+    // Take Standard Input
     lli b, p;
     cin >> b >> p;
-    cout << exponentiation(b, p) << "\n";
+    cout << exponentiation2(b, p) << "\n";
     return 0;
 }
